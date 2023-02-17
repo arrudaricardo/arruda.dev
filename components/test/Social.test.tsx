610b2eb6bc781e171141e7c0125df34ef47a612f
Ricardo de Arruda <settiricardo@gmail.com>
2023-02-17 10:55:11 +0000

    add vitest

---
+++ components/test/Social.test.tsx
@@ -0,0 +1,18 @@
+import { expect, test, describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Social from '../Social'
+
+describe("Social", () => {
+
+  test('no post', () => {
+    render(<Social hasPosts={false} />)
+    expect(screen.queryByText(/posts/i)).toBeNull()
+  })
+
+  test('with post', () => {
+    render(<Social hasPosts={true} />)
+    expect(screen.queryByText(/posts/i)).toBeDefined()
+
+  })
+
+})
