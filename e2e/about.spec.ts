69b5c4d2daaf51d881d0ae4f13c012f38ae80c71
Ricardo de Arruda <ricardo@arruda.dev>
2023-05-21 22:16:38 +0100

    test: update e2e

--- e2e/about.spec.ts
+++ e2e/about.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from "@playwright/test";
+import { createHash } from "node:crypto";
 
-test("generate pdf", async ({ page }) => {
+test("generated about pdf", async ({ page }) => {
   await page.goto("/about");
   const downloadPromise = page.waitForEvent("download");
   await page.getByText("Download").click();
   const download = await downloadPromise;
   expect(download.suggestedFilename()).toMatchSnapshot();
-  const fileStream = await download.createReadStream()
-  // Read all at once
-  const buffer = await new Promise((resolve, reject) => {
+  const fileStream = await download.createReadStream();
+  const buffer = await new Promise<Buffer>((resolve, reject) => {
     const chunks: Uint8Array[] = [];
     fileStream?.on("data", (chunk) => chunks.push(chunk));
     fileStream?.on("error", reject);
     fileStream?.on("end", () => resolve(Buffer.concat(chunks)));
   });
-  expect(buffer).toMatchSnapshot();
+  const pdfHash = createHash("md5")
+    .update(new TextDecoder().decode(buffer))
+    .digest("hex");
+  expect(pdfHash).toMatchSnapshot();
 });
