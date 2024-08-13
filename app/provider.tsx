82fad61310a5d91d9889a7c141ba6dc212c5d4de
Ricardo de Arruda <settiricardo@gmail.com>
2024-08-13 15:14:54 +0100

    env

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,16 +3,12 @@ import { ReactNode } from "react";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== "undefined") {
-  if (
-    !process.env.NEXT_PUBLIC_POSTHOG_KEY ||
-    !process.env.NEXT_PUBLIC_POSTHOG_HOST
-  ) {
-    throw new Error("Posthog key or host not found");
-  }
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY as string;
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST as string;
 
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+if (typeof window !== "undefined") {
+  posthog.init(POSTHOG_KEY, {
+    api_host: POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
